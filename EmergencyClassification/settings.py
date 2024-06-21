ad6ca693f30e1869e7021d666d09375dfd0d33ea
Abdul Rahman <102407007+AbdulRahmanGit@users.noreply.github.com>
2024-06-21 12:03:29 +0530

    Update settings.py

--- EmergencyClassification/settings.py
+++ EmergencyClassification/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+'''STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static'), ]
+'''
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
